e12a60aede7e48a4e0a44f715c70d436d824961a
song <>
2021-04-30 14:33:48 +0900

    update naver

--- app/src/main/java/com/example/webtoon/recyclerview/WebtoonitemViewHolder.kt
+++ app/src/main/java/com/example/webtoon/recyclerview/WebtoonitemViewHolder.kt
@@ -1,29 +1,21 @@
 package com.example.webtoon.recyclerview
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.util.Log
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
-import com.example.webtoon.App
 import com.example.webtoon.Model.WebToonModel
-import com.example.webtoon.R
-import com.example.webtoon.WebToonCollection
 import com.example.webtoon.databinding.WebtoonItemBinding
 import com.squareup.picasso.Picasso
 
-class WebtoonitemViewHolder(val binding : WebtoonItemBinding):
+class WebtoonitemViewHolder(val binding: WebtoonItemBinding):
                                          RecyclerView.ViewHolder(binding.root),
                                          View.OnClickListener {
     private var webtoonUri: String  = ""
-    private val webtoonPhoto = binding.ivImg
+    private val webtoonPhoto = binding.wbImg
     private val webtoonTitle = binding.tvTitle
     private val webtoonArticle = binding.tvArticle
 
@@ -34,9 +26,8 @@ class WebtoonitemViewHolder(val binding : WebtoonItemBinding):
     fun bindWithView(model: WebToonModel) {
         webtoonTitle.text = model.title
         webtoonArticle.text = model.artist
-        Log.d("이미지확인",model.img.toString())
-        Picasso.get().load(model.img).fit().into(webtoonPhoto)
-
+        Log.d("이미지확인", model.img.toString())
+        initWebView(model.img.toString())
 //        Glide.with(App.instance ).load(model.img).placeholder(R.drawable.ic_webloding).into(webtoonPhoto)
         webtoonUri = model.url.toString()
     }
@@ -44,10 +35,28 @@ class WebtoonitemViewHolder(val binding : WebtoonItemBinding):
     override fun onClick(v: View?) {
         when(v) {
             webtoonPhoto -> {
-                Log.d("로그","이미지 클릭")
+                Log.d("로그", "이미지 클릭")
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webtoonUri))
                 v.context.startActivities(arrayOf(intent))
             }
         }
     }
+
+    private fun initWebView(address : String) {
+
+        // 와이파이 & 데이터 연결되어 있으면 웹뷰 생성
+
+            // 인터넷 연결 되어 있을 때 (셀룰러/와이파이)
+        webtoonPhoto.settings.javaScriptEnabled = true // 자바 스크립트 허용
+
+            // 웹뷰안에 새 창이 뜨지 않도록 방지
+        webtoonPhoto.webViewClient = WebViewClient()
+        webtoonPhoto.webChromeClient = WebChromeClient()
+
+            // 원하는 주소를 WebView에 연결
+        webtoonPhoto.loadUrl(address)
+
+
+    }
+
 }
