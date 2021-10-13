d414fcd7d38b4ffebad839011aef7b3e07d61e2e
Hyunsoo Song <68528956+sj960126@users.noreply.github.com>
2021-10-13 21:48:34 +0900

    refactoring :  WebView 로직 수정

--- app/src/main/java/com/example/webtoon/recyclerview/WebtoonitemViewHolder.kt
+++ app/src/main/java/com/example/webtoon/recyclerview/WebtoonitemViewHolder.kt
@@ -26,16 +26,13 @@ class WebtoonitemViewHolder(val binding: WebtoonItemBinding):
     fun bindWithView(model: WebToonModel) {
         webtoonTitle.text = model.title
         webtoonArticle.text = model.artist
-        Log.d("이미지확인", model.img.toString())
         initWebView(model.img.toString())
-//        Glide.with(App.instance ).load(model.img).placeholder(R.drawable.ic_webloding).into(webtoonPhoto)
         webtoonUri = model.url.toString()
     }
 
     override fun onClick(v: View?) {
         when(v) {
             webtoonPhoto -> {
-                Log.d("로그", "이미지 클릭")
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webtoonUri))
                 v.context.startActivities(arrayOf(intent))
             }
@@ -43,20 +40,13 @@ class WebtoonitemViewHolder(val binding: WebtoonItemBinding):
     }
 
     private fun initWebView(address : String) {
-
-        // 와이파이 & 데이터 연결되어 있으면 웹뷰 생성
-
-            // 인터넷 연결 되어 있을 때 (셀룰러/와이파이)
-        webtoonPhoto.settings.javaScriptEnabled = true // 자바 스크립트 허용
-
-            // 웹뷰안에 새 창이 뜨지 않도록 방지
-        webtoonPhoto.webViewClient = WebViewClient()
-        webtoonPhoto.webChromeClient = WebChromeClient()
-
-            // 원하는 주소를 WebView에 연결
-        webtoonPhoto.loadUrl(address)
-
-
+        webtoonPhoto.apply{
+          settings.javaScriptEnabled = true
+          webViewClient = WebViewClient()
+          webChromeClient = WebChromeClient()
+          loadUrl(address)
+        }
+        
     }
 
 }
