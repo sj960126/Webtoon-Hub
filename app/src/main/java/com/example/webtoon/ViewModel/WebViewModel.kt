074ba0b8d4bd43aa04881e1cbfa8aab67e5c20c0
Hyunsoo Song <68528956+sj960126@users.noreply.github.com>
2021-10-13 21:43:22 +0900

    fix :  주석 제거

--- app/src/main/java/com/example/webtoon/ViewModel/WebViewModel.kt
+++ app/src/main/java/com/example/webtoon/ViewModel/WebViewModel.kt
@@ -7,8 +7,7 @@ import com.example.webtoon.Model.WebToonModel
 //데이터 변경 관련
 
 class WebViewModel : ViewModel() {
-    //뮤터블 라이브 데이터 -  수정가능
-    //라이브 데이터 - 값 변경 불가
+ 
     val data: MutableLiveData<ArrayList<WebToonModel>> by lazy {
         MutableLiveData<ArrayList<WebToonModel>>()
     }
